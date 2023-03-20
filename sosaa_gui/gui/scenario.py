b673635b0ba940e1b6cdf59c93efd288e008741a
Juniper Tyree <juniper.tyree@helsinki.fi>
2023-03-20 17:46:18 +0200

    Fix the trajectory length date jump

--- sosaa_gui/gui/scenario.py
+++ sosaa_gui/gui/scenario.py
@@ -61,12 +61,18 @@ def init_scenario_gui(gui):
     gui.modelTypeGroup.idToggled.connect(changeModelType)
 
     def changeTrajectoryDuration():
+        lastFullDays = gui.trajectory_duration.lastValue
         fullDays = gui.trajectory_duration.value()
+
+        gui.trajectory_duration.lastValue = fullDays
 
         if fullDays < 0:
             # Negative duration: start_date = floor(end_date - full_days)
             gui.start_date.setEnabled(False)
             gui.end_date.setEnabled(True)
+
+            if lastFullDays > 0:
+                gui.end_date.setDate(gui.start_date.date())
 
             gui.start_date.setDate(gui.end_date.date().addDays(fullDays))
             gui.start_date.setTime(QtCore.QTime(0, 0))
@@ -76,10 +82,13 @@ def init_scenario_gui(gui):
                 .addSecs(gui.aero_start_offset.value() * 60 * 60)
                 .toString(" dd/MM/yyyy HH:mm:ss")
             )
-        else:
+        elif fullDays > 0:
             # Positive duration: end_date = ceil(start_date + full_days)
             gui.start_date.setEnabled(True)
             gui.end_date.setEnabled(False)
+
+            if lastFullDays < 0:
+                gui.start_date.setDate(gui.end_date.date())
 
             gui.end_date.setDate(
                 gui.start_date.date().addDays(
@@ -87,8 +96,22 @@ def init_scenario_gui(gui):
                 )
             )
             gui.end_date.setTime(QtCore.QTime(0, 0))
+        else:
+            # Zero duration, check which direction we are coming from
+
+            if lastFullDays > 0:
+                gui.end_date.setDate(
+                    gui.start_date.date().addDays(
+                        gui.start_date.time() > QtCore.QTime(0, 0)
+                    )
+                )
+            else:
+                gui.start_date.setDate(gui.end_date.date())
+
+            gui.end_date.setTime(QtCore.QTime(0, 0))
 
     gui.trajectory_duration.valueChanged.connect(changeTrajectoryDuration)
+    gui.trajectory_duration.lastValue = gui.trajectory_duration.value()
 
     def changeAerosolStartOffset():
         gui.aero_start_date.setText(
