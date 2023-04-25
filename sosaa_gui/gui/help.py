0794d33aaa6f0414a16570ea0976743d52bb1cdc
Juniper Tyree <juniper.tyree@helsinki.fi>
2023-04-26 00:11:38 +0300

    Polished the SOSAA GUI icons

--- sosaa_gui/gui/help.py
+++ sosaa_gui/gui/help.py
@@ -5,20 +5,27 @@ from ..resources import resource_path
 
 
 class About(QtWidgets.QDialog):
-    def __init__(self, parent=None):
-        super(About, self).__init__(parent)
+    def __init__(self, gui):
+        super(About, self).__init__()
+
         self.ab = about.Ui_Dialog()
         self.ab.setupUi(self)
         self.ab.okgreat.clicked.connect(self.reject)
         self.setWindowIcon(QtGui.QIcon(resource_path("icons/sosaa-icon.svg")))
         self.ab.logo.setPixmap(
-            QtGui.QPixmap(resource_path("icons/sosaa-splash.svg"))
+            QtGui.QPixmap(
+                resource_path(
+                    "icons/sosaa-splash-dark.svg"
+                    if gui.dark
+                    else "icons/sosaa-splash-light.svg"
+                )
+            )
         )
 
 
 def init_gui_help(gui):
     def openAboutHelp():
-        About().exec()
+        About(gui).exec()
 
     gui.actionAbout_SOSAA.triggered.connect(openAboutHelp)
 
